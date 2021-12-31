0d0c4dba6d2ee031a2c89c16e55487b8d1dd6f04
wpaq <wallysonpabloo@gmail.com>
2021-12-31 12:36:46 -0300

    configs heroku port

--- dist/server.js
+++ dist/server.js
@@ -4,4 +4,4 @@ _dotenv2.default.config();
 var _app = require('./app'); var _app2 = _interopRequireDefault(_app);
 
 const port = process.env.APP_PORT;
-_app2.default.listen(port);
+_app2.default.listen(port || 3000);
