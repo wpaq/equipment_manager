b8d31b1a17e40f1e89ceff5200e87c52c937fad2
wpaq <wallysonpabloo@gmail.com>
2021-12-31 12:59:53 -0300

    config postgres host

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   dialect: 'postgres',
   host: process.env.DATABASE_HOST,
-  // port: process.env.DATABASE_PORT,
+  port: process.env.DATABASE_PORT,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_DATABASE,
