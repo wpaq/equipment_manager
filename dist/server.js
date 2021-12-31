3fe36d08f323af91593860651744d9ae5a0178f4
wpaq <wallysonpabloo@gmail.com>
2021-12-31 12:47:28 -0300

    port

--- dist/server.js
+++ dist/server.js
@@ -3,7 +3,4 @@ _dotenv2.default.config();
 
 var _app = require('./app'); var _app2 = _interopRequireDefault(_app);
 
-const port = process.env.APP_PORT;
-_app2.default.listen(port, "0.0.0.0", function() {
-    console.log(`Listening on Port ${port}`);
-});
+_app2.default.listen(process.env.APP_PORT);
--- src/server.js
+++ src/server.js
@@ -3,7 +3,4 @@ dotenv.config();
 
 import app from './app';
 
-const port = process.env.APP_PORT;
-app.listen(port, "0.0.0.0", function() {
-    console.log(`Listening on Port ${port}`);
-});
+app.listen(process.env.APP_PORT);
