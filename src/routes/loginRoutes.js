14f907962b33f75eae1a4b1b75b40684b583ce30
wpaq <wallysonpabloo@gmail.com>
2022-01-13 21:02:53 -0300

    page login-logado add loginRequired

--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import loginController from '../controllers/LoginController';
+import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
 
-router.get('/', loginController.index);
+router.get('/', loginRequired, loginController.index);
 
 export default router;
