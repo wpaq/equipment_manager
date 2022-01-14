74cfa4f5c65216e58a0617a00e5146674b7e3a1f
wpaq <wallysonpabloo@gmail.com>
2022-01-13 21:10:36 -0300

    loginRedirect err ajust

--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -1,9 +1,8 @@
 import { Router } from 'express';
 import loginController from '../controllers/LoginController';
-import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
 
-router.get('/', loginRequired, loginController.index);
+router.get('/', loginController.index);
 
 export default router;
