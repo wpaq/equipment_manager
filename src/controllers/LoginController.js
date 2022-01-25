6809c6067c8b083df4d61b3b4e1ce42f2de0fc29
wpaq <wallysonpabloo@gmail.com>
2022-01-25 16:04:52 -0300

    ejs title remove

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -1,7 +1,7 @@
 class LoginController {
     async index (req, res) {
         if (!req.session.token) {    
-            return res.render('login', { title:'Login' });           
+            return res.render('login');           
         }
         return res.render('login-logado', { title:'Logado' });              
     }
