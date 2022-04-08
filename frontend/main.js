7cc6055b9d0e18de5ecfa4d2bd64b826bcceb122
wpaq <wallysonpabloo@gmail.com>
2022-04-08 12:49:43 -0300

    criando menu sidebar

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,7 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './assets/css/style.css';
-import './assets/js/bootstrap.bundle.min.js';
