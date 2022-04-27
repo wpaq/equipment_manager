0fef69819b09bc8a20e92b7b666ac027f2f4011d
wpaq <wallysonpabloo@gmail.com>
2022-04-27 16:39:14 -0300

    modal bootstrap and build project

--- frontend/custom.js
+++ frontend/custom.js
@@ -1,6 +1,8 @@
 let btn = document.querySelector('#btn');
 let sidebar = document.querySelector('.sidebar');
 let searchBtn = document.querySelector('.bx-search');
+let deleteConfirm = document.querySelectorAll('.deleteConfirm');
+let deleteLink = document.querySelectorAll('.deleteLink');
 
 let limiter = document.querySelector('.limiter');
 
@@ -15,3 +17,7 @@ searchBtn.addEventListener('click', () => {
 limiter.addEventListener('click', () => {
     limiter.classList.toggle('.limit-active');
 })
+
+for(let i = 0; i < this.deleteLink.length; i++) {
+    console.log(deleteLink[i]);
+}
--- frontend/main.js
+++ frontend/main.js
@@ -1,7 +1,8 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-import 'bootstrap'
+import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 import './assets/css/style.css';
