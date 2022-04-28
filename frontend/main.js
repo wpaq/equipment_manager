69e4e29f879aafd7476f033bed7e5e2abe61c010
wpaq <wallysonpabloo@gmail.com>
2022-04-28 12:55:35 -0300

    build project

--- frontend/main.js
+++ frontend/main.js
@@ -3,6 +3,5 @@ import 'regenerator-runtime/runtime';
 
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 import './assets/css/style.css';
