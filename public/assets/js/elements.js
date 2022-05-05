aa3f95fa0b1d96d02f552334c68e5ac568d64494
wpaq <wallysonpabloo@gmail.com>
2022-05-05 11:41:26 -0300

    qrcode image download

--- public/assets/js/elements.js
+++ public/assets/js/elements.js
@@ -9,7 +9,7 @@ export default {
         this.equipmentDate = document.querySelector('#equipment-date');
         this.qrcodeLink = document.querySelectorAll('.qrcodeLink');
         this.qrcodeImage = document.querySelector('.qrcodeImage');
-        this.printerButton = document.querySelector('.printerButton');
+        this.qrcodeImageLinkDownload = document.querySelector('.qrCodeImageLinkDownload');
     },
 
     actions() {
@@ -35,25 +35,7 @@ export default {
             for(let i = 0; i < this.qrcodeLink.length; i++) {                                
                 this.qrcodeLink[i].onclick = () => {
                     this.qrcodeImage.src = `/assets/img/${this.qrcodeLink[i].id}.png`;
-
-                    if (this.printerButton) {
-                        this.printerButton.addEventListener('click', () => {
-                            var newPage = window.open('about:black', '_new');
-                            newPage.document.open();
-                            newPage.document.write(
-                                '<html>',
-                                '   <head>',
-                                '   </head>',
-                                '   <body>',
-                                '       <img src="' + this.qrcodeImage.src + '" width="200" height="200"/>',
-                                '   </body>',
-                                '</html>'
-                            );
-                            newPage.onload(newPage.window.print())
-                            newPage.onafterprint(newPage.window.close())
-                            
-                        })
-                    }
+                    this.qrcodeImageLinkDownload.href = `/assets/img/${this.qrcodeLink[i].id}.png`
                 }
             }
         }
